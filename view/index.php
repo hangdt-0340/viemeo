d8ab5521333de1260faa43e06d4c3c7c2f2f5244
hangduong <hangaypho@gmail.com>
2020-11-15 16:53:33 +0700

    Create index.php

---
+++ view/index.php
@@ -0,0 +1,75 @@
+<?php include 'C:/xampp1/htdocs/Viemeo/stickers/FrontHeader/FrontHeader.phtml';?>
+
+<body style="margin-top: 50px;">
+<div class="container line-style-1">
+    <div class="row">
+        <div class="col-xl-6">
+            <div>
+                <img src="../media/banner/banner-5.gif">
+            </div>
+        </div>
+        <div class="col-xl-6">
+            <div class="title-head">Where content creation happens</div>
+            <div>
+                <span>Viemeo</span>
+                is a collaborative platform for creating images, videos, and GIFs.
+            </div>
+            <div class="action-button">
+                <div class="start-with-sutdio-btn" data-cy="home-start-editing-button">Start editing
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" style="vertical-align: middle;">
+                        <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
+                    </svg>
+                </div>
+                <div class="home-signin">or, create a workspace</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container line-style-2">
+    <div class="row">
+        <div class="col-xl-5">
+            <div class="title-center">The perfect tool for any content</div>
+            <div class="content-style">
+                <div class="row content-item"><span>Studio</span></div>
+                <div class="row content-item"><span>Subtitles</span></div>
+                <div class="row content-item"><span>Resizer</span></div>
+                <div class="row content-item"><span>Trimmer</span></div>
+                <div class="row content-item"><span>Add Audio</span></div>
+                <div class="row content-item"><span>Explore all tools</span></div>
+            </div>
+        </div>
+        <div class="col-xl-7">
+            <div>
+                <img src="../media/banner/banner-7.gif">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container line-style-3">
+    <div class="row">
+        <div class="col-xl-7">
+            <div>
+                <img class="workspace-preview-img" src="../media/banner/banner-12.png">
+            </div>
+        </div>
+        <div class="col-xl-5">
+            <div class="title-center">All in one workspace to manage assets</div>
+            <div class="work-space-subhead">
+                Upload, edit, and share files with a team in a single place. Secure, backed up, and cross-device - now, work can happen from anywhere.
+            </div>
+            <span class="start-with-sutdio-btn" data-cy="home-start-editing-button">
+                Sign in to create a workspace
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="Home_chevron__1Q1Ye" height="1em" width="1em" style="vertical-align: middle;">
+                    <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
+                </svg>
+            </span>
+        </div>
+    </div>
+</div>
+</body>
+
+<?php include 'C:/xampp1/htdocs/Viemeo/stickers/FrontFooter/FrontFooter.phtml';?>
+
+
